9ce9b4bea291f348fd9cb46db68f7a8b925074dc
Ren <renato.vergara.dev@gmail.com>
2025-06-08 00:03:16 +0200

    feat(Footer): create Footer component for creator info in `src/layout/` folder

---
+++ client/src/layout/Footer.jsx
@@ -0,0 +1,10 @@
+import { FaHeart } from "react-icons/fa";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <p>Renato R. Vergara 2025</p>
+            <p>Made by <span><a href="https://github.com/arcvra" target="blank">@arcvra</a></span> with <span><FaHeart className="fill-red-600" /></span></p>
+        </footer>
+    )
+}
