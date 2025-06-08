a2d2ba68ecdd93d0d396bbe172bc64014e926493
Ren <renato.vergara.dev@gmail.com>
2025-06-08 16:26:40 +0200

    refactor: update database configuration and initialization in config.js

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,12 +1,39 @@
-import pkg from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const Pool = pkg;
-const pool = new Pool({
+export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
 
-export default pool;
+const sql = `
+    CREATE TABLE IF NOT EXISTS severities (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS types (
+        id SERIAL PRIMARY KEY,
+        severity_id INTEGER NOT NULL REFERENCES severities(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS logs (
+        id SERIAL PRIMARY KEY,
+        type_id INTEGER NOT NULL REFERENCES types(id),
+        severity_id INTEGER NOT NULL REFERENCES severities(id),
+        created_at TIMESTAMP DEFAULT NOW()
+    );
+`;
+
+export const initDatabase = async () => {
+    try {
+        await pool.query(sql);
+        console.log("Tablas creadas con éxito");
+    } catch (err) {
+        console.error("Error al ejecutar SQL: ", err);
+    } finally {
+        await pool.end();
+    }
+};
