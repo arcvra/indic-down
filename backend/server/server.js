8be6759a4995d2474c95bc624d4baab07de5af2a
Ren <renato.vergara.dev@gmail.com>
2025-06-09 01:39:07 +0200

    feat(server): implement `getTypes` route in the server

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { getLogs } from "#server/routes/getLogs.js";
+import { getTypes } from "#server/routes/getTypes.js";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cors(corsConfig));
 
 getLogs(app);
+getTypes(app);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}.`)
