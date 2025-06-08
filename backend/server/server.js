690321bbf035a10bb8cda62b83facfe9ea54f02a
Ren <renato.vergara.dev@gmail.com>
2025-06-08 16:26:48 +0200

    feat: initialize database connection in server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { initDatabase } from "#database/config.js";
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 const corsConfig = {
     origin: `${process.env.HOST}:${PORT}`
 }
+
+// Inicia la DB
+initDatabase();
 
 const app = express();
 app.use(express.json());
