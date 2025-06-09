3e098728b2ff6cb578bb581329282b84df1a1475
Ren <renato.vergara.dev@gmail.com>
2025-06-09 02:15:51 +0200

    fix(logs.model): add JSDoc comments for `listLogs` and LogRow typedef

--- backend/server/models/logs.model.js
+++ backend/server/models/logs.model.js
@@ -1,5 +1,11 @@
 import { pool } from "#database/config.js";
-
+/**
+ * Devuelve todos los logs registrados, con su tipo y nivel de severidad, ordenados de más reciente a más antiguo
+ * 
+ * @async
+ * @function listLogs
+ * @returns {Promise<import('pg').QueryResult<LogRow>>} - Resultado de la consulta (rows = LogRow[])
+ */
 export const listLogs = () => pool.query(`
     SELECT 
         logs.id, 
@@ -12,3 +18,12 @@ export const listLogs = () => pool.query(`
     JOIN severities ON types.severity_id = severities.id
     ORDER BY logs.created_at DESC;
      `);
+
+/**
+* @typedef {Object} LogRow
+* @property {number} id
+* @property {string} created_at  Fecha y hora de inserción
+* @property {string} type        Nombre del tipo de error
+* @property {string} severity    Nombre de la severidad
+* @property {string} level       Nivel de severidad (“Bajo, “Moderado", etc.)
+*/
