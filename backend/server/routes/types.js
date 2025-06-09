83db543c0e534c098fbe25d459fd950999199abb
Ren <renato.vergara.dev@gmail.com>
2025-06-09 22:57:50 +0200

    fix(types.route): improve response handling in GET route for types
    - Now returns status and error message as intended

--- backend/server/routes/types.js
+++ backend/server/routes/types.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.get("/", async (req, res) => {
     try {
         const data = await listTypes();
-        res.status(200).json({ status: true, content: data.rows });
+        return res.status(200).json({ status: true, content: data.rows });
     } catch (err) {
         console.error("No se han podido obtener los datos: ", err);
-        res.status(500).json({
+        return res.status(500).json({
             status: false,
             message: "Error al acceder a la base de datos:",
-            error: err
+            error: err.message
         })
     }
 });
