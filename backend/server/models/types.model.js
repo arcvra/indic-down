5ef19da89624b78383c9e01e8ee43910b4921f02
Ren <renato.vergara.dev@gmail.com>
2025-06-09 21:14:02 +0200

    feat(types.model): create `listTypesBySeverity` function to retrieve error types by severity
    - Added JSDoc-style comments for clarity and consitency

--- backend/server/models/types.model.js
+++ backend/server/models/types.model.js
@@ -26,3 +26,32 @@ export const listTypes = () => pool.query(`
 * @property {string} severity    Nombre de la severidad
 * @property {string} level       Nivel de severidad (“Bajo, “Moderado", etc.)
 */
+
+/**
+ * Devuelve la información de tipo de error según nombre de severidad
+ * 
+ * @async
+ * @function listTypesBySeverity
+ * @param {string} severity 
+ * @returns {Promise<import('pg').QueryResult<TypeRow>>}
+ */
+export const listTypesBySeverity = (severity) => pool.query(`
+    SELECT
+        types.id,
+        types.severity_id,
+        types.name AS type,
+        severities.name AS severity,
+        severities.level AS level
+    FROM types
+    JOIN severities ON types.severity_id = severities.id
+    WHERE severities.name = $1
+    `, [severity]);
+
+/**
+* @typedef {Object} TypeRow
+* @property {number} id
+* @property {number} severity_id
+* @property {string} type
+* @property {string} severity
+* @property {string} level
+*/
