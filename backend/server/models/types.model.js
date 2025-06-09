005596db31317e414751dd4cb2454b3162992894
Ren <renato.vergara.dev@gmail.com>
2025-06-09 02:16:07 +0200

    fix(types.model): add JSDoc comments for `listTypes` function and `LogRow` typedef

--- backend/server/models/types.model.js
+++ backend/server/models/types.model.js
@@ -1,5 +1,11 @@
 import { pool } from "#database/config.js";
-
+/**
+* Devuelve todos los tipos de error existentes, con su nombre y nivel de severidad asociado, ordenados de menos a más crítico
+* 
+* @async
+* @function listTypes
+* @returns {Promise<import('pg').QueryResult<LogRow>>}
+*/
 export const listTypes = () => pool.query(`
     SELECT 
         types.id,
@@ -11,3 +17,12 @@ export const listTypes = () => pool.query(`
     JOIN severities ON types.severity_id = severities.id
     ORDER BY types.severity_id ASC;
      `);
+
+/**
+* @typedef {Object} LogRow
+* @property {number} id
+* @property {string} type        Nombre del tipo de error
+* @property {number} severity_id ID del nivel de severidad       
+* @property {string} severity    Nombre de la severidad
+* @property {string} level       Nivel de severidad (“Bajo, “Moderado", etc.)
+*/
