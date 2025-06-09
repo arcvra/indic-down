b8ea7fd0db019a13c36c6051caeca8d0a9e105cb
Ren <renato.vergara.dev@gmail.com>
2025-06-09 16:03:58 +0200

    feat(actionButtons): add click handler to ActionButton for interaction
    - Implements basic `onClick` handler as placeholder
    
    TODO: implement POST action based on `type` props

--- client/src/features/actionButtons/ActionButton.jsx
+++ client/src/features/actionButtons/ActionButton.jsx
@@ -5,12 +5,18 @@ import { useState } from "react";
 export const ActionButton = ({ data }) => {
     const [types, setTypes] = useState(data);
     
+    const handleClick = (e) => {
+        e.preventDefault;
+        alert("Click")
+    }
+    
     return (
         <>
             {types?.map((item) => (
                 <Button
                     key={item.id}
                     ariaLabel="potato"
+                    onClick={handleClick}
                     className="bg-indigo-950 transition-transform hover:scale-95 ease-in-out cursor-pointer"
                     name={item.type}
                     id={item.id}
