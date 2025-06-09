d2faf03aab67f20ea8d5102972ad3c9ab85bb0b1
Ren <renato.vergara.dev@gmail.com>
2025-06-10 01:00:49 +0200

    feat(ActionButton): update `handleClick` to use severity parameter
    - Use inline arrow function to pass severity value from each button
    - Prepares handler to send POST request based on error type

--- client/src/features/actionButtons/ActionButton.jsx
+++ client/src/features/actionButtons/ActionButton.jsx
@@ -4,19 +4,18 @@ import { useState } from "react";
 
 export const ActionButton = ({ data }) => {
     const [types, setTypes] = useState(data);
-    
-    const handleClick = (e) => {
-        e.preventDefault;
-        alert("Click")
+
+    const handleClick = (severity) => {
+        alert(`Clicked ${severity}`);
     }
-    
+
     return (
         <>
             {types?.map((item) => (
                 <Button
                     key={item.id}
                     ariaLabel="potato"
-                    onClick={handleClick}
+                    onClick={() => handleClick(item.severity)}
                     className="bg-indigo-950 transition-transform hover:scale-95 ease-in-out cursor-pointer"
                     name={item.type}
                     id={item.id}
