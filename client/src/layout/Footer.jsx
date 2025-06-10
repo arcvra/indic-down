09622103454fcf03428e1bd7050456111fce2514
Ren <renato.vergara.dev@gmail.com>
2025-06-10 06:07:36 +0200

    feat(footer): enhance layout and styling of the Footer component

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -2,9 +2,9 @@ import { FaHeart } from "react-icons/fa";
 
 export const Footer = () => {
     return (
-        <footer>
-            <p>Renato R. Vergara 2025</p>
-            <p>Made by <span><a href="https://github.com/arcvra" target="blank">@arcvra</a></span> with <span><FaHeart className="fill-red-600" /></span></p>
+        <footer className="grid grid-cols-2 text-xs items-center justify-center text-zinc-800 px-8 py-3 gap-5 md:text-sm">
+            <p className="justify-self-center">Renato R. Vergara 2025</p>
+            <p className="justify-self-center">Made by <span><a href="https://github.com/arcvra" target="blank" className="text-zinc-500">@arcvra</a></span> with <span><FaHeart className="fill-red-600 inline" /></span></p>
         </footer>
     )
 }
