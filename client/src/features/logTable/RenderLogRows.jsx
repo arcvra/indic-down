91cbede4bd4c287a32ca31ac3b9165f4967dd85d
Ren <renato.vergara.dev@gmail.com>
2025-06-10 05:23:34 +0200

    feat(logs): implement `RenderLogRows` component to display fetched log entries
    -Sends fetched data to `LogRow` as props

---
+++ client/src/features/logTable/RenderLogRows.jsx
@@ -0,0 +1,18 @@
+import { LogRow } from "@/components/LogRow";
+import { fetchLogs } from "@/services/fetchLogs"
+
+export const RenderLogRows = async () => {
+    const logs = await fetchLogs();
+    return (
+        <>
+            {logs?.map((log) => (
+                <LogRow
+                    key={log.id}
+                    type={log.type}
+                    severity={log.severity}
+                    createdAt={log.created_at}
+                />
+            ))}
+        </>
+    )
+}
