930ed5f3dd5f409d902126296d6d880c2ad5368e
Ren <renato.vergara.dev@gmail.com>
2025-06-10 06:30:42 +0200

    feat(buttonsSection): wrap ActionButton in a section for improved layout

--- client/src/features/actionButtons/ButtonsSection.jsx
+++ client/src/features/actionButtons/ButtonsSection.jsx
@@ -4,6 +4,8 @@ import { fetchTypes } from "@/services/types";
 export const ButtonsSection = async () => {
     const data = await fetchTypes();
     return (
-        <ActionButton data={data} />
+        <section className="grid grid-cols-3 grid-rows-2 max-h-[100px] md:gap-5 md:grid-cols-6 md:grid-rows-1">
+            <ActionButton data={data} />
+        </section>
     )
 }
