f4506668f9a46b5e1fd59bf043f67c96b1d823b5
Ren <renato.vergara.dev@gmail.com>
2025-06-10 04:02:45 +0200

    fix(cors): update CORS origin to use CLIENT_PORT instead of PORT
    - This enables proper communication with client

--- backend/server/server.js
+++ backend/server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const corsConfig = {
-    origin: `${process.env.HOST}:${PORT}`
+    origin: `${process.env.HOST}:${process.env.CLIENT_PORT}`
 }
 
 // App middlewares
