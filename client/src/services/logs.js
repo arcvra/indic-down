72b4cee68f7c0e3c92a02248c2549d958b4c01d4
Ren <renato.vergara.dev@gmail.com>
2025-06-12 01:35:45 +0200

    refactor(services/logs): unify fetch and post logic in a single file for simplicity
    - Unified `postLogs`  and `fetchLogs` functions in `services/logs.js` file
    
    BREAKING CHANGE: deprecated `services/fetchLogs.js` and `services/postLogs.js` and moved functions into `services/logs.js`

--- client/src/services/logs.js
+++ client/src/services/logs.js
@@ -37,3 +37,29 @@ export const postLog = async (typeId) => {
         return null;
     }
 }
+
+/** 
+ * Obtiene todos los registros de error.
+ * 
+ * @async
+ * @function fetchLogs
+ * @returns {Promise<Object|null>}
+ */
+
+export const fetchLogs = async () => {
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+    try {
+        const res = await fetch(`${API_URL}/logs`);
+        if (!res.ok) {
+            throw new Error("Error de servidor: " + res.status);
+        }
+
+        const data = await res.json();
+        return data.content;
+    } catch (err) {
+        console.error("Error al hacer fetch: ", err);
+        return null;
+    }
+
+}
