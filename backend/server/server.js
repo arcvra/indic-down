0e1b0aea9d92e3d47e2ccac75da45ebd88472034
Ren <renato.vergara.dev@gmail.com>
2025-06-14 23:35:17 +0200

    fix(server): uncomment PORT assignment for proper server configuration
    - TEST

--- backend/server/server.js
+++ backend/server/server.js
@@ -5,7 +5,7 @@ import setupRoutes from "#server/routes/index.js";
 
 dotenv.config();
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // const corsConfig = {
 //     origin: `${process.env.HOST}`,
