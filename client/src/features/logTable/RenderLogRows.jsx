671ee4f8920ed0082887b02cd133ba6a5bacc07a
Ren <renato.vergara.dev@gmail.com>
2025-06-14 23:48:52 +0200

    fix(logs): add error handling for fetchLogs to display message on failure

--- client/src/features/logTable/RenderLogRows.jsx
+++ client/src/features/logTable/RenderLogRows.jsx
@@ -3,6 +3,11 @@ import { fetchLogs } from "@/services/logs";
 
 export const RenderLogRows = async () => {
     const logs = await fetchLogs();
+
+    if (!logs) {
+        return (<p> Algo no funciona </p>);
+    }
+
     return (
         <>
             {logs?.map((log) => (
