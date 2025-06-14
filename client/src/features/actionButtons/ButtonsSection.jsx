ba0a9e4de7db1dcacf6419d19f0773eb4e4e86e8
Ren <renato.vergara.dev@gmail.com>
2025-06-14 18:23:35 +0200

    fix(ButtonsSection): increase max height of section and add gap for better layout

--- client/src/features/actionButtons/ButtonsSection.jsx
+++ client/src/features/actionButtons/ButtonsSection.jsx
@@ -4,7 +4,7 @@ import { fetchTypes } from "@/services/types";
 export const ButtonsSection = async () => {
     const data = await fetchTypes();
     return (
-        <section className="grid grid-cols-3 grid-rows-2 max-h-[100px] md:gap-5 md:grid-cols-6 md:grid-rows-1">
+        <section className="grid grid-cols-3 grid-rows-2 gap-2 max-h-[200px] md:gap-5 md:grid-cols-6 md:grid-rows-1">
             <ActionButton data={data} />
         </section>
     )
