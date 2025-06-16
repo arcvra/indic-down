3552eaed283aec3c7c17879d1331606b08bca5e4
Renato <renato.vergara.dev@gmail.com>
2025-06-16 14:41:52 +0200

    feat(RenderLogRows): enhance loading and error states with LogRow components
    - Improved loading, error and no data messages appearance
    - Added icons for improved styling

--- client/src/features/logTable/RenderLogRows.jsx
+++ client/src/features/logTable/RenderLogRows.jsx
@@ -2,15 +2,35 @@
 import useSWR from "swr";
 import { LogRow } from "@/components/LogRow";
 import { fetchLogs } from "@/services/logs";
+import { TbMoodLookDown, TbMoodSadDizzy, TbMoodHappy } from "react-icons/tb";
 
 export const RenderLogRows = () => {
     const { data: logs, error, isLoading } = useSWR("logs", fetchLogs);
 
-    if (isLoading) return <p>Cargando registros...</p>;
-    if (error) return <p>No ha sido posible cargar los registros :/</p>
+    if (isLoading) return (
+        <LogRow
+            type="Cargando registros."
+            severity={<TbMoodLookDown />}
+            createdAt="--"
+        />
+    )
 
-    if (!logs || logs.length === 0) return <p> No hay registros aún. </p>;
+    if (error) return (
+        <LogRow
+            type="No ha sido posible cargar los registros."
+            severity={<TbMoodSadDizzy />}
+            createdAt="--"
+        />
+    )
 
+
+    if (!logs || logs.length === 0) return (
+        <LogRow
+            type="No hay registros aún."
+            severity={<TbMoodHappy />}
+            createdAt="--"
+        />
+    )
 
     return (
         <>
