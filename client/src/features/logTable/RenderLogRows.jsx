897d753b20f953a8dce220f9bf0e17d11dfc5058
Renato <renato.vergara.dev@gmail.com>
2025-06-16 11:57:22 +0200

    feat(RenderLogs): update RenderLogRows to use SWR for data fetching and loading/error states
    - Renders different content when loading/error state
    - Gets updated when `ActionButton` posts a new log

--- client/src/features/logTable/RenderLogRows.jsx
+++ client/src/features/logTable/RenderLogRows.jsx
@@ -1,12 +1,16 @@
+"use client"
+import useSWR from "swr";
 import { LogRow } from "@/components/LogRow";
 import { fetchLogs } from "@/services/logs";
 
-export const RenderLogRows = async () => {
-    const logs = await fetchLogs();
+export const RenderLogRows = () => {
+    const { data: logs, error, isLoading } = useSWR("logs", fetchLogs);
 
-    if (!logs) {
-        return (<p> Algo no funciona </p>);
-    }
+    if (isLoading) return <p>Cargando registros...</p>;
+    if (error) return <p>No ha sido posible cargar los registros :/</p>
+
+    if (!logs || logs.length === 0) return <p> No hay registros aún. </p>;
+
 
     return (
         <>
