89daa8e9a2cd054e76acefd0ecf8925a0595d631
Renato <renato.vergara.dev@gmail.com>
2025-06-19 18:41:07 +0200

    refactor(ActionButton.jsx): simplify disabled check in handleClick function

--- client/src/features/actionButtons/ActionButton.jsx
+++ client/src/features/actionButtons/ActionButton.jsx
@@ -36,7 +36,7 @@ export const ActionButton = ({ data }) => {
     };
 
     const handleClick = async (itemId) => {
-        if (isDisabled == true) return;
+        if (isDisabled) return;
 
         try {
             await postLog(itemId);
