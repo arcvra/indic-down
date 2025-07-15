ea4fb4efffd3c255c4cef1b7197d95d463ff416f
Renato <renato.vergara.dev@gmail.com>
2025-07-15 15:40:06 +0200

    refactor(ButtonsSection): migrate component to TypeScript and improve error handling
    - Adds type checking to API response (APItypeResponse)
    - Validates response status and content length before rendering
    - Displays fallback message if data can't be fetched

---
+++ client/src/features/actionButtons/ButtonsSection.tsx
@@ -0,0 +1,22 @@
+import { ActionButton } from "@/features/actionButtons/ActionButton";
+import { APItypeResponse, fetchTypes, TypeItem } from "@/services/types";
+
+export const ButtonsSection = async () => {
+    const data: APItypeResponse = await fetchTypes();
+
+    if (!data.status || !Array.isArray(data.content) || data.content.length == 0) {
+        return (
+            <section>
+                <p>No hemos podido cargar la información de esta sección. Por favor, inténtalo más tarde.</p>
+            </section>
+        )
+    }
+
+    const issuesTypes: TypeItem[] = data.content;
+
+    return (
+        <section className="grid grid-cols-3 grid-rows-2 gap-2 max-h-[200px] md:gap-5 md:grid-cols-6 md:grid-rows-1">
+            <ActionButton data={issuesTypes} />
+        </section>
+    )
+}
