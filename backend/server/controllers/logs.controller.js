c58a3524827132a7d22f592ee520c2e12072959c
Renato Vergara <renato.vergara.dev@gmail.com>
2025-09-15 18:04:49 +0200

    refactor(logs): move insertLogs logic to a separate controller for improved structure
    - Now the controller manages the HTTP request and handles data
    - Simplifies route usage and readability

--- backend/server/controllers/logs.controller.js
+++ backend/server/controllers/logs.controller.js
@@ -1,4 +1,4 @@
-import { listLogs } from "#server/models/logs.model.js";
+import { listLogs, insertLog } from "#server/models/logs.model.js";
 
 export const listLogsController = async (req, res) => {
     try {
@@ -9,6 +9,33 @@ export const listLogsController = async (req, res) => {
         return res.status(500).json({
             status: false,
             message: "Error al acceder a la base de datos:",
+            error: err.message
+        })
+    }
+}
+
+export const insertLogsController = async (req, res) => {
+    try {
+        const { typeId } = req.body;
+
+        if (typeof typeId !== "number") {
+            return res.status(400).json({
+                status: false,
+                message: "Parámetros inválidos. Se esperaba {typeId: number}."
+            });
+        }
+
+        const result = await insertLog(typeId);
+        return res.status(201).json({
+            status: true,
+            content: result.rows[0]
+        });
+
+    } catch (err) {
+        console.error("No se ha podido completar la solicitud: ", err);
+        return res.status(500).json({
+            status: false,
+            message: "Error al acceder a la base de datos.",
             error: err.message
         })
     }
--- backend/server/routes/logs.js
+++ backend/server/routes/logs.js
@@ -1,36 +1,10 @@
 import express from "express";
-import { insertLog } from "#server/models/logs.model.js";
-import { listLogsController } from "#server/controllers/logs.controller.js";
+import { insertLogsController, listLogsController } from "#server/controllers/logs.controller.js";
 
 const router = express.Router();
 
 router.get("/", listLogsController);
 
-router.post("/", async (req, res) => {
-    try {
-        const { typeId } = req.body;
-
-        if (typeof typeId !== "number") {
-            return res.status(400).json({
-                status: false,
-                message: "Parámetros inválidos. Se esperaba {typeId: number}."
-            });
-        }
-
-        const result = await insertLog(typeId);
-        return res.status(201).json({
-            status: true,
-            content: result.rows[0]
-        });
-
-    } catch (err) {
-        console.error("No se ha podido completar la solicitud: ", err);
-        return res.status(500).json({
-            status: false,
-            message: "Error al acceder a la base de datos.",
-            error: err.message
-        })
-    }
-})
+router.post("/", insertLogsController);
 
 export default router;
