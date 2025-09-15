35784b0a29faaa69ce221a280154b1d980ad13c2
Renato Vergara <renato.vergara.dev@gmail.com>
2025-09-15 22:41:48 +0200

    feat(logs): enhance listLogsController to support pagination with limit and offset parameters
    - Controller recieves query strings and validates values
    - Defaults limit and offset to 20 and 0
    - Returns response with new pagination field

--- backend/server/controllers/logs.controller.js
+++ backend/server/controllers/logs.controller.js
@@ -2,8 +2,27 @@ import { listLogs, insertLog } from "#server/models/logs.model.js";
 
 export const listLogsController = async (req, res) => {
     try {
-        const data = await listLogs();
-        return res.status(200).json({ status: true, content: data.rows });
+        // Se extraen los parámetros de la query string con valores por defecto (20, 0)
+        let { limit = 20, offset = 0 } = req.query;
+
+        // Se convierten parámetros a números enteros
+        limit = parseInt(limit, 10);
+        offset = parseInt(offset, 10);
+
+        /* Se validan los valores recibidos. 
+        En caso de no ser válidos, revierte al predeterminado */
+        if (isNaN(limit) || limit < 1) limit = 20;
+        if (isNaN(offset) || offset < 0) offset = 0;
+
+        // Consulta de la base de datos
+        const data = await listLogs(limit, offset);
+
+        // Se devuelve respuesta con datos de paginación
+        return res.status(200).json({
+            status: true,
+            content: data.rows,
+            pagination: { limit, offset }
+        });
     } catch (err) {
         console.error("No se han podido obtener los datos: ", err);
         return res.status(500).json({
